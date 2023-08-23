342f535c8ff0a5f2f9115bdf2b29d73c33af16c9
vyPal <kubik.palacky@gmail.com>
2023-08-23 20:33:25 +0200

    Add sentry monitoring

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,42 +1,48 @@
 package main
 
 import (
-	"DislikesSchool/EduPage2-server/edupage"
+	"fmt"
 	"time"
 
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-contrib/cache"
 	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 )
 
+type LoginData struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Server   string `json:"server"`
+}
+
 func main() {
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn:              "https://9f278010f63fd37cc43bee40a6d69aa6@o4504950085976064.ingest.sentry.io/4505752992743424",
+		EnableTracing:    true,
+		TracesSampleRate: 1.0,
+	}); err != nil {
+		fmt.Printf("Sentry initialization failed: %v", err)
+	}
+
 	router := gin.Default()
+	router.Use(sentrygin.New(sentrygin.Options{}))
 	store := persistence.NewInMemoryStore(time.Second)
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	
-	router.GET("/login", cache.CachePage(store, time.Minute, func(c *gin.Context) {
-		username, password, server := c.Query("username"), c.Query("password"), c.Query("server")
-		var handle edupage.Handle
-		var err error
-		if server == "" {
-			handle, err = edupage.LoginAuto(username, password)
-		} else {
-			handle, err = edupage.Login(server, username, password)
-		}
-		if err != nil {
-			c.JSON(200, gin.H{
-				"message": "failed",
+
+	router.POST("/login", cache.CachePage(store, time.Minute, func(c *gin.Context) {
+		// Detect if the content is form-urlencoded or json, and use the appropriate binding function
+		var loginData LoginData
+		if err := c.ShouldBind(&loginData); err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
 			})
-		} else {
-			data := handle.RefreshUser()
-			c.JSON(200, gin.H{
-				"message": "success",
-				"user":    data.UserRow.Firstname + " " + data.UserRow.Lastname,
-			})
+			return
 		}
 	}))
 
