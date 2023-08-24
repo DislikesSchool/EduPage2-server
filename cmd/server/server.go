704c759cf7b32f5a1c203125cf2dd726c7551175
vyPal <kubik.palacky@gmail.com>
2023-08-24 06:36:18 +0200

    Added logic for route /login

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/DislikesSchool/EduPage2-server/edupage"
 	"github.com/getsentry/sentry-go"
 	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-contrib/cache"
@@ -36,14 +38,36 @@ func main() {
 	})
 
 	router.POST("/login", cache.CachePage(store, time.Minute, func(c *gin.Context) {
-		// Detect if the content is form-urlencoded or json, and use the appropriate binding function
 		var loginData LoginData
-		if err := c.ShouldBind(&loginData); err != nil {
-			c.JSON(400, gin.H{
-				"error": err.Error(),
-			})
+		if err := c.Bind(&loginData); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+
+		if loginData.Username == "" || loginData.Password == "" {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Username and Password are required"})
+			return
+		}
+
+		var h edupage.Handle
+		var err error
+		if loginData.Server == "" {
+			h, err = edupage.LoginAuto(loginData.Username, loginData.Password)
+		} else {
+			h, err = edupage.Login(loginData.Server, loginData.Username, loginData.Password)
+		}
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		}
+
+		data := h.RefreshUser()
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "success",
+			"name":    data.UserRow.Firstname + " " + data.UserRow.Lastname,
+		})
 	}))
 
 	router.Run()
