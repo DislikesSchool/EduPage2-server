b06081d4ba3dfc16974bcacd1450c3fb1fb525e6
Martin Prejsa <martin.prejsa1@gmail.com>
2023-09-02 17:53:09 +0200

    code revisions, documentation update

--- edupage/edupage.go
+++ edupage/edupage.go
@@ -7,8 +7,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"regexp"
-	"strconv"
+	"path"
+	"reflect"
 	"time"
 
 	"github.com/DislikesSchool/EduPage2-server/edupage/model"
@@ -19,12 +19,13 @@ type EdupageClient struct {
 	Credentials Credentials
 	gsechash    string
 
-	user      *model.User
-	timeline  *model.Timeline
-	results   *model.Results
-	timetable *model.Timetable
+	user *model.User
+	//timeline  *model.Timeline
+	//results   *model.Results
+	//timetable *model.Timetable
 }
 
+// CreateClient is used to create a client struct
 func CreateClient(credentials Credentials) (EdupageClient, error) {
 	var client EdupageClient
 	if credentials.httpClient == nil {
@@ -32,7 +33,7 @@ func CreateClient(credentials Credentials) (EdupageClient, error) {
 	}
 	client.Credentials = credentials
 
-	user, err := client.loadUser()
+	user, err := client.fetchUser()
 
 	if err != nil {
 		return EdupageClient{}, err
@@ -47,7 +48,7 @@ func CreateClient(credentials Credentials) (EdupageClient, error) {
 // If update is set to true, user data wil explicitly update
 func (client *EdupageClient) GetUser(update bool) (model.User, error) {
 	if client.user == nil || update {
-		user, err := client.loadUser()
+		user, err := client.fetchUser()
 		if err != nil {
 			return model.User{}, err
 		}
@@ -59,9 +60,9 @@ func (client *EdupageClient) GetUser(update bool) (model.User, error) {
 	}
 }
 
-// GetTimeline retrieves last 30 days of timeline from edupage.
-func (client *EdupageClient) GetTimeline() (model.Timeline, error) {
-	timeline, err := client.loadTimeline(time.Now().AddDate(0, 0, -30), time.Now())
+// GetRecentTimeline retrieves last 30 days of timeline from edupage.
+func (client *EdupageClient) GetRecentTimeline() (model.Timeline, error) {
+	timeline, err := client.fetchTimeline(time.Now().AddDate(0, 0, -30), time.Now())
 	if err != nil {
 		return model.Timeline{}, err
 	}
@@ -70,8 +71,8 @@ func (client *EdupageClient) GetTimeline() (model.Timeline, error) {
 }
 
 // GetUser retrieves the timeline in a specified time interval from edupage.
-func (client *EdupageClient) GetTimelineFrom(from, to time.Time) (model.Timeline, error) {
-	tt, err := client.loadTimeline(from, to)
+func (client *EdupageClient) GetTimeline(from, to time.Time) (model.Timeline, error) {
+	tt, err := client.fetchTimeline(from, to)
 	if err != nil {
 		return model.Timeline{}, err
 	}
@@ -86,6 +87,7 @@ func (client *EdupageClient) GetRecentResults() (model.Results, error) {
 }
 
 // GetResults retrieves the results in a specified interval from edupage.
+// Halfyears types are: P1 (first halfyear), P2 (second halfyear), RX (whole year)
 func (client *EdupageClient) GetResults(year, halfyear string) (model.Results, error) {
 	results, err := client.fetchResults(year, halfyear)
 	if err != nil {
@@ -105,210 +107,179 @@ func (client *EdupageClient) GetTimetable() (model.Timetable, error) {
 	return tt, nil
 }
 
-// LoadTimeline loads the timeline data from the specified date range.
-func (client *EdupageClient) loadTimeline(datefrom, dateto time.Time) (model.Timeline, error) {
-	url := fmt.Sprintf("https://%s/timeline/?akcia=getData", client.Credentials.Server)
+var (
+	ErrorUnitialized = errors.New("unitialized")
+	ErrorNotFound    = errors.New("not found")
+)
 
-	form, err := CreatePayload(map[string]string{
-		"datefrom": datefrom.Format("2006-01-02"),
-		"dateto":   dateto.Format("2006-01-02"),
-	})
-
-	if err != nil {
-		return model.Timeline{}, fmt.Errorf("failed to create payload: %s", err)
+// GetStudentID is used to retrieve the client's student ID.
+// Returns ErrorUnitialized if the user object hasn't been initialized.
+func (client *EdupageClient) GetStudentID() (string, error) {
+	if client.user == nil {
+		return "", ErrorUnitialized
 	}
-
-	response, err := client.Credentials.httpClient.PostForm(url, form)
-	if err != nil {
-		return model.Timeline{}, ErrAuthorization // most likely case
-	}
-
-	if response.StatusCode != 200 {
-		return model.Timeline{}, fmt.Errorf("server returned code:%d", response.StatusCode)
-	}
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return model.Timeline{}, fmt.Errorf("failed to read response body: %s", err)
-	}
-
-	decoded_body := make([]byte, base64.StdEncoding.DecodedLen(len(body)-4))
-
-	_, err = base64.StdEncoding.Decode(decoded_body, body[4:])
-	if err != nil {
-		return model.Timeline{}, fmt.Errorf("failed to decode response body: %s", err)
-	}
-
-	decoded_body = bytes.Trim(decoded_body, "\x00")
-	timeline, err := model.ParseTimeline(decoded_body)
-	if err != nil {
-		return model.Timeline{}, fmt.Errorf("failed to parse timeline json into json object: %s", err)
-	}
-
-	return timeline, nil
+	return client.user.UserRow.StudentID, nil
 }
 
-// LoadUser loads the user data
-func (client *EdupageClient) loadUser() (model.User, error) {
-	u := fmt.Sprintf("https://%s/user/?", client.Credentials.Server)
-
-	response, err := client.Credentials.httpClient.Get(u)
-	if err != nil {
-		return model.User{}, ErrAuthorization // most likely case
+// GetSubjectByID is used to retrieve the subject by it's specified ID.
+// Returns ErrorNotFound if the subject can't be found.
+// Returns ErrorUnitialized if the user object hasn't been initialized.
+func (client *EdupageClient) GetSubjectByID(id string) (model.Subject, error) {
+	if client.user == nil {
+		return model.Subject{}, ErrorUnitialized
 	}
 
-	if response.StatusCode != 200 {
-		return model.User{}, fmt.Errorf("server returned code: %d", response.StatusCode)
+	if teacher, ok := client.user.DBI.Subjects[id]; ok {
+		return teacher, nil
 	}
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return model.User{}, fmt.Errorf("failed to read response body: %s", err)
-	}
-
-	hash, err := findGSCEhash(body)
-	if err != nil {
-		return model.User{}, fmt.Errorf("failed to parse user json into json object: %s", err)
-	}
-
-	client.gsechash = hash
-
-	js, err := findUserHome(body)
-	if err != nil {
-		return model.User{}, fmt.Errorf("failed to parse user json into json object: %s", err)
-	}
-
-	var user model.User
-	err = json.Unmarshal([]byte(js), &user)
-	if err != nil {
-		return model.User{}, fmt.Errorf("failed to parse user json into json object: %s", err)
-	}
-
-	return user, nil
+	return model.Subject{}, ErrorNotFound
 }
 
-// LoadResults loads the grade data from specified year and halfyear
-// Halfyears types are: P1 (first halfyear), P2 (second halfyear), RX (whole year)
-func (client *EdupageClient) fetchResults(year, halfyear string) (model.Results, error) {
-	url := fmt.Sprintf("https://%s/znamky/?what=studentviewer&akcia=studentData&eqav=1&maxEqav=7", client.Credentials.Server)
-
-	form, err := CreatePayload(map[string]string{
-		"pohlad":           "podladatumu",
-		"znamky_yearid":    year,
-		"znamky_yearid_ns": "1",
-		"nadobdobie":       halfyear,
-		"rokobdobie":       fmt.Sprintf("%s::%s", year, halfyear),
-		"doRq":             "1",
-		"what":             "studentviewer",
-		"updateLastView":   "0",
-	})
-
-	if err != nil {
-		return model.Results{}, fmt.Errorf("failed to create payload: %s", err)
+// GetTeacherByID is used to retrieve the teacher by their specified ID.
+// Returns ErrorNotFound if the teacher can't be found.
+// Returns ErrorUnitialized if the user object hasn't been initialized.
+func (client *EdupageClient) GetTeacherByID(id string) (model.Teacher, error) {
+	if client.user == nil {
+		return model.Teacher{}, ErrorUnitialized
 	}
 
-	response, err := client.Credentials.httpClient.PostForm(url, form)
-	if err != nil {
-		return model.Results{}, ErrAuthorization // most likely case
+	if teacher, ok := client.user.DBI.Teachers[id]; ok {
+		return teacher, nil
 	}
-
-	if response.StatusCode != 200 {
-		return model.Results{}, fmt.Errorf("server returned code:%d", response.StatusCode)
-	}
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return model.Results{}, fmt.Errorf("failed to read response body: %s", err)
-	}
-
-	decoded_body := make([]byte, base64.StdEncoding.DecodedLen(len(body)-4))
-
-	_, err = base64.StdEncoding.Decode(decoded_body, body[4:])
-	if err != nil {
-		return model.Results{}, fmt.Errorf("failed to decode response body: %s", err)
-	}
-
-	decoded_body = bytes.Trim(decoded_body, "\x00")
-
-	results, err := model.ParseResults(decoded_body)
-	if err != nil {
-		return model.Results{}, fmt.Errorf("failed to parse results: %s", err)
-	}
-
-	return results, nil
+	return model.Teacher{}, ErrorNotFound
 }
 
-func (client *EdupageClient) fetchTimetable(datefrom, dateto time.Time) (model.Timetable, error) {
-	u := fmt.Sprintf("https://%s/timetable/server/currenttt.js?__func=curentttGetData", client.Credentials.Server)
-
-	id, err := client.GetStudentID()
-	if err == ErrorUnitialized {
-		return model.Timetable{}, errors.New("failed to create request, user is not initialized")
+// GetClassroomByID is used to retrieve the classroom by it's specified ID.
+// Returns ErrorNotFound if the classroom can't be found.
+// Returns ErrorUnitialized if the user object hasn't been initialized.
+func (client *EdupageClient) GetClassroomByID(id string) (model.Classroom, error) {
+	if client.user == nil {
+		return model.Classroom{}, ErrorUnitialized
 	}
 
-	year, _ := strconv.Atoi(datefrom.Format("2006"))
-
-	request := map[string]interface{}{
-		"__args": []map[string]interface{}{
-			nil,
-			{
-				"year":                 year,
-				"datefrom":             datefrom.Format(model.TimeFormatYearMonthDay),
-				"dateto":               dateto.Format(model.TimeFormatYearMonthDay),
-				"table":                "students",
-				"id":                   id,
-				"showColors":           false,
-				"showOrig":             true,
-				"showIgroupsInClasses": false,
-				"log_module":           "CurrentTTView",
-			},
-		},
-		"__gsh": client.gsechash,
+	if teacher, ok := client.user.DBI.Classrooms[id]; ok {
+		return teacher, nil
 	}
-
-	request_body, err := json.Marshal(request)
-	if err != nil {
-		return model.Timetable{}, fmt.Errorf("failed to create request: %s", err)
-	}
-
-	response, err := client.Credentials.httpClient.Post(u, "application/json", bytes.NewBuffer(request_body))
-	if err != nil {
-		return model.Timetable{}, ErrAuthorization // most likely case
-	}
-
-	if response.StatusCode != 200 {
-		return model.Timetable{}, fmt.Errorf("server returned code: %d", response.StatusCode)
-	}
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return model.Timetable{}, fmt.Errorf("failed to read response body: %s", err)
-	}
-
-	tt, err := model.ParseTimetable(body)
-	if err != nil {
-		return model.Timetable{}, fmt.Errorf("failed to read response body: %s", err)
-	}
-
-	return tt, nil
+	return model.Classroom{}, ErrorNotFound
 }
 
-func findGSCEhash(body []byte) (string, error) {
-	rg, _ := regexp.Compile(`ASC\.gsechash="(.*)";`)
-	matches := rg.FindAllStringSubmatch(string(body), -1)
-	if len(matches) == 0 {
-		return "", errors.New("gsechash not found in the document body")
+// FetchHomeworkAttachmens obtains the homework attchments for the specified homework.
+// Returns ErrUnobtainableAttachments in case the attachments are not present.
+// Retruns map, key is the resource name and value is the resource link
+func (client *EdupageClient) FetchHomeworkAttachments(i *model.Homework) (map[string]string, error) {
+	if len(i.ESuperID) == 0 || len(i.TestID) == 0 {
+		return nil, errors.New("required fields superid and testid not set")
 	}
 
-	return matches[0][1], nil
-}
-
-func findUserHome(body []byte) (string, error) {
-	rg, _ := regexp.Compile(`\.userhome\((.*)\);`)
-	matches := rg.FindAllStringSubmatch(string(body), -1)
-	if len(matches) == 0 {
-		return "", errors.New("userhome not found in the document body")
+	data := map[string]string{
+		"testid":  i.TestID,
+		"superid": i.ESuperID,
 	}
 
-	return matches[0][1], nil
+	payload, err := CreatePayload(data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create payload: %w", err)
+	}
+
+	resp, err := client.Credentials.httpClient.PostForm(
+		"https://"+path.Join(client.Credentials.Server, "elearning", "?cmd=MaterialPlayer&akcia=getETestData"),
+		payload,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("homework request failed: %w", err)
+	}
+
+	response, err := io.ReadAll(resp.Body)
+
+	if len(response) < 5 {
+		return nil, fmt.Errorf("homework request failed, bad response: %w", err)
+	}
+
+	response = response[4:]
+
+	decoded := make([]byte, base64.StdEncoding.DecodedLen(len(response)))
+	_, err = base64.StdEncoding.Decode(decoded, response)
+	if err != nil {
+		return nil, fmt.Errorf("homework request failed, bad response: %w", err)
+	}
+
+	decoded = bytes.Trim(decoded, "\x00")
+	var object map[string]interface{}
+	err = json.Unmarshal(decoded, &object)
+	if err != nil {
+		return nil, fmt.Errorf("homework request failed, bad response: %w", err)
+	}
+
+	attachments := make(map[string]string)
+
+	// God help those who may try to debug this.
+	if object["materialData"] == nil ||
+		(reflect.TypeOf(object["materialData"]).Kind() != reflect.Map ||
+			reflect.TypeOf(object["materialData"]).Elem().Kind() != reflect.Interface) {
+		return nil, model.ErrUnobtainableAttachments
+	}
+	materialData := object["materialData"].(map[string]interface{})
+
+	if materialData["cardsData"] == nil ||
+		(reflect.TypeOf(materialData["cardsData"]).Kind() != reflect.Map ||
+			reflect.TypeOf(materialData["cardsData"]).Elem().Kind() != reflect.Interface) {
+		return nil, model.ErrUnobtainableAttachments
+	}
+	cardsData := materialData["cardsData"].(map[string]interface{})
+
+	for _, entry := range cardsData {
+		if entry == nil ||
+			(reflect.TypeOf(entry).Kind() != reflect.Map ||
+				reflect.TypeOf(entry).Elem().Kind() != reflect.Interface) {
+			return nil, model.ErrUnobtainableAttachments
+		}
+
+		if e, ok := entry.(map[string]interface{})["content"]; !ok && reflect.TypeOf(e).Kind() != reflect.String {
+			return nil, model.ErrUnobtainableAttachments
+		}
+
+		var content map[string]interface{}
+		contentJson := entry.(map[string]interface{})["content"].(string)
+		err = json.Unmarshal([]byte(contentJson), &content)
+		if err != nil {
+			return nil, err
+		}
+
+		if content["widgets"] == nil ||
+			(reflect.TypeOf(content["widgets"]).Kind() != reflect.Slice ||
+				reflect.TypeOf(content["widgets"]).Elem().Kind() != reflect.Interface) {
+			return nil, model.ErrUnobtainableAttachments
+		}
+
+		widgets := content["widgets"].([]interface{})
+		for _, widget := range widgets {
+			if widget == nil ||
+				(reflect.TypeOf(widget).Kind() != reflect.Map ||
+					reflect.TypeOf(widget).Elem().Kind() != reflect.Interface) {
+				return nil, model.ErrUnobtainableAttachments
+			}
+			if widget.(map[string]interface{})["props"] == nil ||
+				(reflect.TypeOf(widget.(map[string]interface{})["props"]).Kind() != reflect.Map ||
+					reflect.TypeOf(widget.(map[string]interface{})["props"]).Elem().Kind() != reflect.Interface) {
+				return nil, model.ErrUnobtainableAttachments
+			}
+			props := widget.(map[string]interface{})["props"].(map[string]interface{})
+			if files, ok := props["files"]; ok {
+				for _, file := range files.([]interface{}) {
+					if file == nil ||
+						(reflect.TypeOf(file).Kind() != reflect.Map ||
+							reflect.TypeOf(file).Elem().Kind() != reflect.Interface) {
+						return nil, model.ErrUnobtainableAttachments
+					}
+					attachments[file.(map[string]interface{})["name"].(string)] = file.(map[string]interface{})["src"].(string)
+				}
+			}
+		}
+		if err != nil {
+			continue
+		}
+		continue
+	}
+
+	return attachments, nil
 }
--- edupage/readme.md
+++ edupage/readme.md
@@ -21,4 +21,22 @@ if err != nil {
 ```
 This is the most important part, the `EdupageClient` struct takes care of data management and fetching.
 
+## Timeline
+To retrieve timeline information use function `EdupageClient#GetRecentTimeline` or `EdupageClient#GetTimeline(Time, Time)`
 
+```golang
+timeline, err := client.GetRecentTimeline()
+if err != nil {
+    //Proper error handling...
+}
+```
+This will retrieve the last 30 days from the timeline.
+
+Or you can specifiy your own interval, like so
+
+```golang
+timeline, err := client.GetTimeline(TIME_FROM, TIME_TO)
+if err != nil {
+    //Proper error handling...
+}
+```
