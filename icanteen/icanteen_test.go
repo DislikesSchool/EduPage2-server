3f8cc617385a6ed433214cd83917d84aa0bf2e28
vyPal <66716025+vyPal@users.noreply.github.com>
2023-11-13 12:41:57 +0100

    Add tests for getting lunches

--- icanteen/icanteen_test.go
+++ icanteen/icanteen_test.go
@@ -1,1 +1,23 @@
 package icanteen
+
+import (
+	"flag"
+	"testing"
+)
+
+var (
+	server   = flag.String("server", "", "Server URL")
+	username = flag.String("username", "", "Username")
+	password = flag.String("password", "", "Password")
+)
+
+func TestGetLunches(t *testing.T) {
+	lunches, err := LoadLunches(*username, *password, *server)
+	if err != nil {
+		t.Fatalf("Failed to get lunches: %s", err)
+	}
+
+	if len(lunches) == 0 {
+		t.Fatalf("No lunches found")
+	}
+}
