da752faf33a3d5249f57d29113269aa7b9063bb2
vyPal <kubik.palacky@gmail.com>
2025-04-15 10:21:16 +0200

    Fix unique keys

--- cmd/server/dbmodel/user.go
+++ cmd/server/dbmodel/user.go
@@ -9,8 +9,8 @@ type User struct {
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
 	Username      string    `gorm:"unique;not null"`
-	Password      string    `gorm:"unique;not null"`
-	Server        string    `gorm:"unique;not null"`
+	Password      string    `gorm:"not null"`
+	Server        string    `gorm:"not null"`
 	LastOnline    time.Time `gorm:"not null"`
 	StoreMessages bool      `gorm:"not null"`
 	StoreTimeline bool      `gorm:"not null"`
